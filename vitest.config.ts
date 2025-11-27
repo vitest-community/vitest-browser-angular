08b9b988a785837e9f6d623fd9d6d46f9de7416e
Marco Rinaldi <marco.rinaldi9@live.com>
2025-11-27 01:44:37 +0100

    Refactor tsconfig for globals imports in tests and vitest configuration for zoneless testing setup

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,26 @@
 import { playwright } from '@vitest/browser-playwright';
-import { defineConfig } from 'vitest/config';
+import { defineConfig, defaultExclude } from 'vitest/config';
 export default defineConfig({
   test: {
+    projects: [
+      {
+        extends: true,
+        test: {
+          name: { label: 'zone', color: 'red' },
+          setupFiles: ['./src/setup-zones.ts'],
+          exclude: [...defaultExclude, '**/zoneless.test.ts'],
+        },
+      },
+      {
+        extends: true,
+        test: {
+          name: { label: 'zoneless', color: 'magenta' },
+          setupFiles: ['./src/setup-zoneless.ts'],
+          include: ['**/zoneless.test.ts'],
+        },
+      },
+    ],
     globals: true,
-    setupFiles: ['./src/setup-zones.ts'],
     browser: {
       enabled: true,
       provider: playwright(),
